702dd76ca1fde467cbca87afc91e7f6cb72360b5
Ryan Meline <ryan.c.meline@gmail.com>
2026-04-25 01:09:39 -0700

    Init network class header

---
+++ include/network.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "layer.h"
+#include <vector>
+
+class Network {
+    public:
+        void add_layer(int inputs, int outputs,
+                    std::function<Eigen::VectorXf(const Eigen::VectorXf&)> act,
+                    std::function<Eigen::VectorXf(const Eigen::VectorXf&)> act_derivative
+                        = [](const Eigen::VectorXf& x) { return x; });
+        Eigen::VectorXf forward(const  Eigen::VectorXf& input);
+        void backward(const Eigen::VectorXf& gradient, float learning_rate);
+        void train(const Eigen::VectorXf& input, uint8_t label, float learning_rate);
+        int predict(const Eigen::VectorXf& input);
+        float eval(const std::vector<Eigen::VectorXf>& images, const std::vector<uint8_t>& labels);
+}
