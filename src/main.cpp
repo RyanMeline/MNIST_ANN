3b7daa0342995c40ab8a906ffbcdfd3ae2d52409
Ryan Meline <ryan.c.meline@gmail.com>
2026-04-24 19:01:04 -0700

    Added MNIST ASCII art

--- src/main.cpp
+++ src/main.cpp
@@ -27,17 +27,28 @@ void print_image(const Eigen::VectorXf& image) {
 }
 
 int main() {
+    std::cout <<"\n" <<
+    "____________________________________________________\n\n" <<
+    " /$$      /$$ /$$   /$$ /$$$$$$  /$$$$$$  /$$$$$$$$" << std::endl <<
+    "| $$$    /$$$| $$$ | $$|_  $$_/ /$$__  $$|__  $$__/" << std::endl <<
+    "| $$$$  /$$$$| $$$$| $$  | $$  | $$  \\__/   | $$   " << std::endl <<
+    "| $$ $$/$$ $$| $$ $$ $$  | $$  |  $$$$$$    | $$   " << std::endl <<
+    "| $$  $$$| $$| $$  $$$$  | $$   \\____  $$   | $$   " << std::endl <<
+    "| $$\\  $ | $$| $$\\  $$$  | $$   /$$  \\ $$   | $$   " << std::endl <<
+    "| $$ \\/  | $$| $$ \\  $$ /$$$$$$|  $$$$$$/   | $$   " << std::endl <<
+    "|__/     |__/|__/  \\__/|______/ \\______/    |__/   " << std::endl <<
+    "____________________________________________________\n\n";
+                                                   
+                                                   
+                                                   
     std::cout << "Loading training data set\n";
-    load_data::Dataset train_data = load_data::read_input(train_data_path, train_label_path);
-
-    std::cout << "\nLabel: " << (int)train_data.labels[0];
-    print_image(train_data.images[0]);
-
+    //load_data::Dataset train_data = load_data::read_input(train_data_path, train_label_path);
+    // std::cout << "\nLabel: " << (int)train_data.labels[0];
+    // print_image(train_data.images[0]);
     std::cout << "Loading test data set\n";
-    load_data::Dataset test_data = load_data::read_input(test_data_path, test_label_path);
-
-    std::cout << "\nLabel: " << (int)test_data.labels[0];
-    print_image(test_data.images[0]);
+    //load_data::Dataset test_data = load_data::read_input(test_data_path, test_label_path);
+    // std::cout << "\nLabel: " << (int)test_data.labels[0];
+    // print_image(test_data.images[0]);
 
 
     return 0;
