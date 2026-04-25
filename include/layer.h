a68a5be9aed24e6b337a069b7a4addb7567e45a8
Ryan Meline <ryan.c.meline@gmail.com>
2026-04-25 00:45:50 -0700

    Initial Layer object and functions

---
+++ include/layer.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <Eigen/Dense>
+#include <functional>
+#include <vector>
+
+class Layer {
+    public:
+        Layer(int inputs, int outputs, 
+              std::function<Eigen::VectorXf(const Eigen::VectorXf&)> act,
+              std::function<Eigen::VectorXf(const Eigen::VectorXf&)> act_derivative 
+                = [](const Eigen::VectorXf& x) { return x; }); //set a default for output layer (might be an issue since it could just square everything, might need to just return 1)
+        Eigen::VectorXf forward(const Eigen::VectorXf& input);
+        Eigen::VectorXf backward(const Eigen::VectorXf& gradient, float learning_rate);
+
+    private:
+        Eigen::MatrixXf W; //weights
+        Eigen::VectorXf b; //biases
+        Eigen::VectorXf x; //stored input
+        Eigen::VectorXf z; //stored pre-activation
+        std::function<Eigen::VectorXf(const Eigen::VectorXf&)> activation;
+        std::function<Eigen::VectorXf(const Eigen::VectorXf&)> activation_derivative;
+};
--- src/layer.cpp
+++ src/layer.cpp
@@ -13,3 +13,32 @@ Need to cache:
 
 W and b update via gradiant descent (handled in network.cpp)
 */
+#include "../include/layer.h"
+
+Layer::Layer(int inputs, int outputs,
+             std::function<Eigen::VectorXf(const Eigen::VectorXf&)> act,
+             std::function<Eigen::VectorXf(const Eigen::VectorXf&)> act_deriv)
+    : activation(act), activation_derivative(act_deriv) {
+        //He initilization for ReLU
+    W = (Eigen::MatrixXf::Random(outputs, inputs) * std::sqrt(2.0f / inputs));
+    b = Eigen::VectorXf::Zero(outputs);
+}
+
+Eigen::VectorXf Layer::forward(const Eigen::VectorXf& input) {
+    x = input;
+    z = W*x + b; //Eigen handles the matrix x vector math
+    return activation(z);
+}
+
+Eigen::VectorXf Layer::backward(const Eigen::VectorXf& gradient, float learning_rate) {
+    Eigen::VectorXf dLdz = gradient.array() * activation_derivative(z).array(); //Make the activation derivative function
+    Eigen::MatrixXf dLdW = dLdz.matrix() * x.matrix().transpose(); //outer product, flips the x matrix
+    Eigen::VectorXf dLdb = dLdz.matrix();
+    Eigen::VectorXf dLdx = W.transpose() * dLdz.matrix();
+
+    //Update weights
+    W = W - learning_rate * dLdW;
+    b = b - learning_rate * dLdb;
+
+    return dLdx;
+}
