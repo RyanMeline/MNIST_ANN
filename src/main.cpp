ca142a3f1327e522b2b8f03d826d6ac4e6d2e200
Ryan Meline <ryan.c.meline@gmail.com>
2026-04-25 03:51:42 -0700

    Added training and testing for network

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,12 @@ void print_image(const Eigen::VectorXf& image) {
     }
 }
 
+using namespace::activation_function;
+
+void runModel() {
+
+}
+
 int main() {
     std::cout <<"\n" <<
     "____________________________________________________\n\n" <<
@@ -39,17 +45,34 @@ int main() {
     "|__/     |__/|__/  \\__/|______/ \\______/    |__/   " << std::endl <<
     "____________________________________________________\n\n";
                                                    
-                                                   
-                                                   
+                                                                                            
     std::cout << "Loading training data set\n";
     load_data::Dataset train_data = load_data::read_input(train_data_path, train_label_path);
-    // std::cout << "\nLabel: " << (int)train_data.labels[0];
-    // print_image(train_data.images[0]);
+
     std::cout << "Loading test data set\n";
     load_data::Dataset test_data = load_data::read_input(test_data_path, test_label_path);
-    // std::cout << "\nLabel: " << (int)test_data.labels[0];
-    // print_image(test_data.images[0]);
 
+    Network network;
+    network.add_layer(784, 128, relu, relu_back);
+    network.add_layer(128, 64, relu, relu_back);
+    network.add_layer(64, 10, softmax);
+
+    float learning_rate = 0.005f;
+    int epochs = 20;
+
+    for(int i = 1; i <= epochs; i++) {
+        std::cout << "Epoch [" << i << "/" << epochs << "]\n"; 
+        for(size_t j = 0; j < train_data.images.size(); j++) {
+            network.train(train_data.images[j], train_data.labels[j], learning_rate);
+
+            if((j+1)%100 == 0) {
+                std::cout << "\r Training Images: [" << j+1 << "/" << train_data.images.size() << "]";
+            }
+        }
+        std::cout << std::endl;
+        float accuracy = network.eval(test_data.images, test_data.labels) * 100;
+        std::cout << "Accuracy: " << accuracy << "%\n\n";
+    }
 
     return 0;
 }
