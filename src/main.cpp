1622ac40c1ac429693df2894ce8988302a33a279
Ryan Meline <ryan.c.meline@gmail.com>
2026-04-24 18:21:38 -0700

    Added function to print images

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,33 @@ const std::string train_label_path = "data/mnist/train-labels.idx1-ubyte";
 const std::string test_data_path = "data/mnist/t10k-images.idx3-ubyte";
 const std::string test_label_path = "data/mnist/t10k-labels.idx1-ubyte";
 
+//For testing
+void print_image(const Eigen::VectorXf& image) {
+    for(int i = 0; i < 28; i++) {
+        for(int j = 0; j < 28; j++) {
+            float pixel = image(i * 28 + j);
+            if(pixel > 0.75f)      std::cout << "@";
+            else if(pixel > 0.5f)  std::cout << "#";
+            else if(pixel > 0.25f) std::cout << ".";
+            else                   std::cout << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
 int main() {
     std::cout << "Loading training data set\n";
     load_data::Dataset train_data = load_data::read_input(train_data_path, train_label_path);
+
+    std::cout << "\nLabel: " << (int)train_data.labels[0];
+    print_image(train_data.images[0]);
+
     std::cout << "Loading test data set\n";
     load_data::Dataset test_data = load_data::read_input(test_data_path, test_label_path);
+
+    std::cout << "\nLabel: " << (int)test_data.labels[0];
+    print_image(test_data.images[0]);
+
+
     return 0;
 }
